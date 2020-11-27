9fcc0bea20055ae3e636836f5764a511283b6807
edinsonrequena <edinsonrequena@protonmail.com>
2020-11-27 14:17:20 -0400

    adding type-any.ts

---
+++ src/type-any.ts
@@ -0,0 +1,15 @@
+// Tipo explicito
+let idUser: any
+idUser = 1 // number
+idUser = '1' // string
+console.log('idUser', idUser)
+
+// Tipo inferido
+let otherId
+otherId = 1
+otherId = '1'
+console.log('otherId', outerHeight)
+
+let surprise: any = 'Hello type'
+const res = surprise.substring(6)
+console.log('res', res)
